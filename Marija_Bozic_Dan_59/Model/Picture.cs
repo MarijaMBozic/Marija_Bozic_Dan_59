e009983170fc1238d97883eac2050b83f5835c7d
MarijaMBozic <60043521+MarijaMBozic@users.noreply.github.com>
2020-09-02 14:17:35 +0200

    add pictures

---
+++ Marija_Bozic_Dan_59/Model/Picture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marija_Bozic_Dan_59.Model
+{
+    public class Picture
+    {
+        public int PictureId { get; set; }
+        public string Name { get; set; }
+    }
+}
